232c748c92be55a19f53793ddfe7a129e0e951cc
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-14 12:26:59 -0600

    added sample function

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 function add(x:number,y:number) {
     return x + y;
+} 
+
+//acepta un tipo, recibe un array de ese tipo y devuelve algo de ese mismo tipo
+function sample<T>(arr: T[]): T {
+    const idx = Math.floor(Math.random() * arr.length);
+    return arr[idx]
 }
