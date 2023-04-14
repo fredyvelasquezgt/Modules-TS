dd45a925f330a692342b31b58e34fdef2ac80d4b
Fredy Velasquez <macacoloco696@gmail.com>
2023-04-14 12:31:17 -0600

    dist created

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+sample([1, 2, 3, 4]);
+add(1, 2);
--- dist/utils.js
+++ dist/utils.js
@@ -1,11 +1,10 @@
-function add(x:number,y:number) {
+"use strict";
+function add(x, y) {
     return x + y;
-} 
-
-//acepta un tipo, recibe un array de ese tipo y devuelve algo de ese mismo tipo
-function sample<T>(arr: T[]): T {
-    const idx = Math.floor(Math.random() * arr.length);
-    return arr[idx]
 }
-
+//acepta un tipo, recibe un array de ese tipo y devuelve algo de ese mismo tipo
+function sample(arr) {
+    const idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+}
 const x = 1;
